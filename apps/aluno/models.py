2d6374ba59cab80a9fb928149881f1c3fe04bf43
larakimbelyifce <lara.kimbely.pereira60@aluno.ifce.edu.br>
2021-09-21 22:24:39 -0300

    second

--- apps/aluno/models.py
+++ apps/aluno/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length=100,help_text='Nome do Aluno')
-    cpf = models.CharField(max_length=12,help_text='CPF do Aluno',blank=False,default='')
+    nome = models.CharField(max_length=100,help_text='Nome')
+    cpf = models.CharField(max_length=12,help_text='CPF',blank=False,default='')
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='perfil',default='')
-    email = models.CharField(max_length=100,help_text='e-mail',default='')
+    email = models.CharField(max_length=100,help_text='E-mail',default='')
     data_nasc = models.DateField('Data de Nascimento',auto_now=False,auto_now_add=False,blank=True,default='')
 
 class Meta:
